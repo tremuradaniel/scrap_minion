989503915c6aa6d679fb7a323ef5a7947fec3d01
Daniel Tremură <tremuradaniel@gmail.com>
2022-12-17 12:20:36 +0000

    MS-8 - adding delay to scraping

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 from selenium import webdriver
 import os
+import time
+import random
 from dotenv import load_dotenv
 load_dotenv()
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+
+def main():
+  simulateRandomAccess()
+  driver = get_driver()
+  print(getPrice(driver))
 
 def get_driver():
   service = Service(os.environ.get('PATH_TO_CHROME_DRIVER'))
@@ -16,19 +23,30 @@ def get_driver():
   options.add_argument("no-sandbox")
   options.add_argument("disable-blink-features=AutomationControlled")
   options.add_experimental_option("excludeSwitches", ["enable-automation"])
-  
+  options.add_argument("--headless")
   driver = webdriver.Chrome(service=service, options=options)
   
   driver.get(os.environ.get('WEBSITE_ADDRESS') + os.environ.get('WEBSITE_PATH'))
   
   return driver
 
-def main():
-  driver = get_driver()
-  print(getPrice(driver))
+"""
+  simulateRandomAccess() - delay the running of the script so that 
+  the access will not be made each day in order to reduce the 
+  suspicion of scraping 
+"""
+def simulateRandomAccess():
+  time.sleep(minutesToSecond(getRandomIntForMax(os.environ.get('MAX_WAITING'))))
+  
+  
+def getRandomIntForMax(int):
+  return round(random.random() * int)
+
+def minutesToSecond(minutes):
+  return minutes * 60
   
 def getPrice(driver):
-  element = driver.find_element(By.XPATH ,os.environ.get('WEBSITE_XPATH'))
+  element = driver.find_element(By.XPATH, os.environ.get('WEBSITE_XPATH'))
   return element.text.split(" ")[0]
 
 main()
