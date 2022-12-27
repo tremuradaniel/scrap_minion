dd0de8a7b86368e21f66aa5ea68ec97e01f4eb30
Daniel Tremură <tremuradaniel@gmail.com>
2022-12-27 14:25:22 +0000

    [MS-10] added logging

---
+++ LoggerClass.py
@@ -0,0 +1,19 @@
+import os
+import logging
+from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class LoggerClass:
+  def __init__(self) -> None:
+    logging.basicConfig( 
+      filename=Path(os.environ.get('LOG_FILE')),
+      filemode='a',
+      level=logging.DEBUG,
+      format= '%(asctime)s - %(levelname)s - %(message)s',
+    )
+    
+  def logError(self, error) -> None:
+    logging.exception(str(error))
+  
